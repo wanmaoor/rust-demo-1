da6f929cfa26256698465b9c3801d5d1345bd888
wanmaoor <wanmao1284879375@outlook.com>
2019-12-22 22:35:05 +0800

    array

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,4 +1,22 @@
+use std::mem;
 pub fn run() {
-  let numbers: [i32; 5] = [1, 2, 3, 4, 5];
-  println!("{:?}", numbers)
+  let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+  println!("{:?}", numbers);
+
+  // Get single val
+  println!("Single value 1:{}", numbers[0]);
+
+  // Reassign val
+  numbers[0] = 11;
+  println!("Single value 1:{}", numbers[0]);
+
+  // Get array length
+  println!("Array length: {}", numbers.len());
+
+  // Arrays are stack allocated
+  println!("Array occupies {} bytes", mem::size_of_val(&numbers));
+
+  // Get slice
+  let slice: &[i32] = &numbers[0..3];
+  println!("Slice: {:?}", slice);
 }
