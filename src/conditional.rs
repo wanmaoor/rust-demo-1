9ad3792083275af65918548d04d843f1ee2cb825
wanmaoor <wanmao1284879375@outlook.com>
2019-12-23 08:54:42 +0800

    conditional

--- src/conditional.rs
+++ src/conditional.rs
@@ -1,8 +1,16 @@
-pub fn run(){
+pub fn run() {
   let age = 23;
-
+  let check_id = false;
   // if else
-  if age > 18 {
+  if age > 18 && check_id {
     println!("I'm a adult");
+  } else if age < 18 && check_id {
+    println!("Little kids");
+  } else {
+    println!("balala");
   }
+
+  // short if
+  let is_of_age = if 10 > 8 { true } else { false };
+  println!("Is of Age {}", is_of_age);
 }
